d7d30b022238feeed6fc3cbc7dc22b7ed09efb1d
James <james@Jamess-MacBook-Air.local>
2015-08-06 20:43:15 -0600

    Step 2a: quick check of the teens

--- tst/number_converter_test.rb
+++ tst/number_converter_test.rb
@@ -6,6 +6,10 @@ class TestNumberConverter < Test::Unit::TestCase
     assert_equal("One", NumberConverter.convert(1), "1 should convert to 'One'")
   end
   
+  def test_twelve
+    assert_equal("Twelve", NumberConverter.convert(12), "12 should convert to 'Twelve'")
+  end
+  
   def test_twenty_five
     assert_equal("Twenty-five", NumberConverter.convert(25), "25 should convert to 'Twenty-five'")
   end
